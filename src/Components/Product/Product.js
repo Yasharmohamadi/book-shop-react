62a88da38b02b9a2dda8a629de5a07fc0a73a03a
Yashar <yashar22mo@gmail.com>
2024-07-14 13:22:50 +0330

    Add Product codes

---
+++ src/Components/Product/Product.js
@@ -0,0 +1,9 @@
+import './Product.css'
+
+export default function Product () {
+    return (
+        <section className="items">
+            <h2 className="items_title">کالا ها</h2>
+        </section>
+    )
+}
